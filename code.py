12d48c61e64059aebebec689ed75810f8e649c49
Simon Reed <info@net-impact.net>
2020-10-11 18:41:09 -0600

    Start of code

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
 
 def do_something():
-    x = "Starting point for code.py"
+    x = "1. Starting point for code.py"
     print(x)# this will make it much easier in future problems to see that something is actually happening
